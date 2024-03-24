a0db766ec1df23681a3f9295ae835523a99f2de1
brain-breaker <163159516+brain-breaker@users.noreply.github.com>
2024-03-24 14:33:24 +0300

    Update test_items.py
    add the assert statement in test_items.py

--- test_items.py
+++ test_items.py
@@ -5,4 +5,5 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_guest_should_see_button_add_to_basket(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    button = browser.find_elements(By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    assert button, 'button add to basket is not presented'
